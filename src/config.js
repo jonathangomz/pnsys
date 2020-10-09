eeed58edbe40ed1b80965f0ad88d0aab525638ff
Jonathan <jonicgp97@gmail.com>
2020-10-08 20:04:36 -0500

    Initial configuration

---
+++ src/config.js
@@ -0,0 +1,65 @@
+/* eslint-disable no-unused-vars */
+import path from 'path'
+import merge from 'lodash/merge'
+
+/* istanbul ignore next */
+const requireProcessEnv = (name) => {
+  if (!process.env[name]) {
+    throw new Error('You must set the ' + name + ' environment variable')
+  }
+  return process.env[name]
+}
+
+/* istanbul ignore next */
+if (process.env.NODE_ENV !== 'production') {
+  const dotenv = require('dotenv-safe')
+  dotenv.config({
+    path: path.join(__dirname, '../.env'),
+    example: path.join(__dirname, '../.env.example')
+  })
+}
+
+const config = {
+  all: {
+    env: process.env.NODE_ENV || 'development',
+    root: path.join(__dirname, '..'),
+    port: process.env.PORT || 9000,
+    ip: process.env.IP || '0.0.0.0',
+    apiRoot: process.env.API_ROOT || '',
+    defaultEmail: 'no-reply@um-push.com',
+    sendgridKey: requireProcessEnv('SENDGRID_KEY'),
+    masterKey: requireProcessEnv('MASTER_KEY'),
+    jwtSecret: requireProcessEnv('JWT_SECRET'),
+    mongo: {
+      options: {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true
+      }
+    },
+    oneSignal: {
+      authKey: requireProcessEnv('AUTH_KEY'),
+      restApiKey: requireProcessEnv('REST_API_KEY'),
+      appId: requireProcessEnv('APP_ID'),
+    }
+  },
+  test: { },
+  development: {
+    mongo: {
+      uri: requireProcessEnv('MONGODB_URI'),
+      options: {
+        debug: true
+      }
+    }
+  },
+  production: {
+    ip: process.env.IP || undefined,
+    port: process.env.PORT || 8080,
+    mongo: {
+      uri: requireProcessEnv('MONGODB_URI') || 'mongodb://localhost/um-push'
+    }
+  }
+}
+
+module.exports = merge(config.all, config[config.all.env])
+export default module.exports
